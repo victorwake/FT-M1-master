9c18ed0946a78b1d3ddf545c36d060209a013f5e
victorwake <victorpintowake@gmail.com>
2022-08-01 23:12:42 -0300

    IntroToCS Ok

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,14 +2,32 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+// de binario a decimales
+    let numero = num.split('');
+    let resultado = 0;
+    let exponente = numero.length - 1;
+    for (let i = 0; i < numero.length; i++) {
+        resultado += numero[i] * Math.pow(2, exponente);
+        exponente--;
+    }
+    return resultado;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+// de decimales a binario
+    let numero = num;
+    let resultado = '';
+    while (numero > 0) {
+        resultado = (numero % 2) + resultado;
+        numero = Math.floor(numero / 2);
+    }
+    return resultado;
 
 }
 
+console.log(BinarioADecimal('10011')); 
+console.log(DecimalABinario(19));
 
 module.exports = {
   BinarioADecimal,
