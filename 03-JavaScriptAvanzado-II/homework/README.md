afcdc08cfc7b5d60cb2fa10243e83663ddcb026a
victorwake <victorpintowake@gmail.com>
2022-08-10 00:35:57 -0300

    03-JavaScriptAvanzado-II

--- 03-JavaScriptAvanzado-II/homework/README.md
+++ 03-JavaScriptAvanzado-II/homework/README.md
@@ -15,7 +15,17 @@ Para ejecutar los test, anda a esta carpeta y ejecutá:
 
 Crear un método `repeatify` que este disponible para _todos_ los objetos `Strings`. Esta función debe aceptar un `entero` que indica cuantas veces el string tiene que repetirse. La función retorna el string repetido el número de veces que indicamos. Controlar que el número no sea menor que cero, y si es cero que devuelva `''` (String vacío).
 
+
 ```javascript
+ function repeatify(numero) {
+        if (numero < 0) {
+            return "";
+        } else {
+            return this.repeat(numero);
+        }
+}
+
+String.prototype.repeatify = repeatify;
 console.log('hola'.repeatify(3));   //holaholahola
 ```
 
@@ -28,6 +38,23 @@ console.log('hola'.repeatify(3));   //holaholahola
 Probá tu solución con el siguiente código:
 
 ```javascript
+ let shape = {
+        type: "",
+        getType: function () {
+            return this.type;
+        }
+    };
+function triangle(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.type = "Triangle";
+}
+triangle.prototype = shape;
+triangle.prototype.getPerimeter = function () {
+    return this.a + this.b + this.c;
+}
+
 > var t = new Triangle(1, 2, 3);
 > t instanceof Triangle
 // true
@@ -44,6 +71,15 @@ Probá tu solución con el siguiente código:
 Probá tu solución con el siguiente código:
 
 ```javascript
+function circle(radius) {
+    this.radius = radius;
+    this.type = "Circle";
+}
+circle.prototype = shape;
+circle.prototype.getPerimeter = function () {
+    return 2 * Math.PI * this.radius;
+}
+
 > var c = new Circle(2);
 > c instanceof Circle
 // true
--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -19,15 +19,27 @@ function counter() {
   otroContador()      // 2
   otroContador()      // 3
    */
+  var contador = 0;
+  return function () {
+    contador++;
+    return contador;
+  }
 }
 
 function cacheFunction(cb) {
   /*
   Ejercicio 2
 
-  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
+  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback 
+  que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada 
+  operación que hace, de manera que, al realizar una operación por segunda vez, se pueda obtener el resultado de 
+  esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
 
-  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
+  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese 
+  rgumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación 
+  (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente 
+    invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario 
+  volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
 
 
   Ejemplo:
@@ -38,9 +50,17 @@ function cacheFunction(cb) {
   const squareCache = cacheFunction(square)
 
   squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
-  squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
+  squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el 
+  resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
 
   */
+const objeto = {};
+return function (arg) {
+  if (!objeto.hasOwnProperty(arg)) {
+    objeto[arg] = cb(arg);
+  }
+  return objeto[arg];
+}
 }
 
 // Bind
@@ -64,25 +84,42 @@ function getNombre() {
 
   IMPORTANTE: no modificar el código de arriba (variables instructor y alumno, y función getNombre)
 
-  Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
+  Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que 
+  actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor;
-let getNombreAlumno;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
   
-  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
+  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones 
+  que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). 
+  Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
 */
 
 function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
   return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos;
-let textoGuiones;
-let textoUnderscore;
+let textoAsteriscos = crearCadena.bind(this, "*", "*");
+
+let textoGuiones = crearCadena.bind(this, "-", "-");
+
+let textoUnderscore = crearCadena.bind(this, "_", "_");
+
+// let textoAsteriscos = function (cadena) {
+//   return crearCadena("*", "*", cadena);
+// }
+// let textoGuiones = function (cadena) {
+//   return crearCadena("-", "-", cadena);
+// }
+// let textoUnderscore = function (cadena) {
+//   return crearCadena("_", "_", cadena);
+// }
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
